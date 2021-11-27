6648b6c4d314cec99531debf6fe445039edb7e96
pozitp <61188367+pozitp@users.noreply.github.com>
2021-11-27 20:46:22 +0700

    Enhanced notifications

--- src/main/java/com/github/pozitp/mixin/ModMenuNotificationCheck.kt
+++ src/main/java/com/github/pozitp/mixin/ModMenuNotificationCheck.kt
@@ -1,23 +1,37 @@
 package com.github.pozitp.mixin
 
+import com.hughbone.fabrilousupdater.platform.ModPlatform
+import net.minecraft.client.MinecraftClient
 import net.minecraft.client.gui.screen.TitleScreen
+import net.minecraft.client.toast.SystemToast
+import net.minecraft.entity.player.PlayerEntity
+import net.minecraft.text.Text
 import org.spongepowered.asm.mixin.Mixin
 import org.spongepowered.asm.mixin.injection.At
 import org.spongepowered.asm.mixin.injection.Inject
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo
+import java.util.*
+
 
 @Mixin(TitleScreen::class)
 class ModMenuNotificationCheck {
-    private companion object {
-        private var showed: Boolean = false;
-    }
+    private var shownUpdateNotification:Boolean = false
 
-    @Inject(at = [At("RETURN")], method = ["render"])
+    @Inject(at = [At("TAIL")], method = ["render"])
     fun init(info: CallbackInfo) {
-        if (!showed) {
-            println("Hello, world!")
-            // TODO: Check updates
+        if (shownUpdateNotification) {
+            return
         }
-        showed = true;
+        shownUpdateNotification = true
+//        val player: PlayerEntity? = null
+//        ModPlatform().start(player, "update");
+        // TODO: Check updates
+        Objects.requireNonNull(MinecraftClient.getInstance()).toastManager.add(
+            SystemToast(
+                SystemToast.Type.TUTORIAL_HINT,
+                Text.of("Updates Available!"),
+                Text.of("${1} mods can be updated")
+            )
+        )
     }
 }
