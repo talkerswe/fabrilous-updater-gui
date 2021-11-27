642be43962feedf5a02cf156e00c4ca5f5f7da8b
pozitp <61188367+pozitp@users.noreply.github.com>
2021-11-27 13:53:23 +0700

    Convert to Kotlin

--- src/main/java/com/github/pozitp/config/ModMenuConfig.kt
+++ src/main/java/com/github/pozitp/config/ModMenuConfig.kt
@@ -1,23 +1,21 @@
-package com.github.pozitp.config;
+package com.github.pozitp.config
 
-import com.terraformersmc.modmenu.api.ConfigScreenFactory;
-import com.terraformersmc.modmenu.api.ModMenuApi;
-import me.shedaniel.clothconfig2.api.ConfigBuilder;
-import me.shedaniel.clothconfig2.api.ConfigCategory;
-import net.minecraft.text.Text;
+import com.terraformersmc.modmenu.api.ConfigScreenFactory
+import com.terraformersmc.modmenu.api.ModMenuApi
+import me.shedaniel.clothconfig2.api.ConfigBuilder
+import me.shedaniel.clothconfig2.api.ConfigCategory
+import net.minecraft.client.gui.screen.Screen
+import net.minecraft.text.Text
 
-public class ModMenuConfig implements ModMenuApi {
-    @Override
-    public ConfigScreenFactory<?> getModConfigScreenFactory() {
-        return parent -> {
-            ConfigBuilder builder = ConfigBuilder.create()
-                    .setParentScreen(parent)
-                    .setTitle(Text.of("Whoami"));
-
-            ConfigCategory general = builder.getOrCreateCategory(Text.of("General"));
-            builder.setFallbackCategory(general);
-            return builder.build();
-
-        };
+class ModMenuConfig : ModMenuApi {
+    override fun getModConfigScreenFactory(): ConfigScreenFactory<*> {
+        return ConfigScreenFactory { parent: Screen? ->
+            val builder = ConfigBuilder.create()
+                .setParentScreen(parent)
+                .setTitle(Text.of("Whoami"))
+            val general = builder.getOrCreateCategory(Text.of("General"))
+            builder.setFallbackCategory(general)
+            builder.build()
+        }
     }
 }
