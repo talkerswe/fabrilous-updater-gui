589f2289cd489cc31c0074a560250862bda8db00
pozitp <61188367+pozitp@users.noreply.github.com>
2021-11-28 19:23:09 +0700

    Update to latest locale version

--- src/main/kotlin/com/github/pozitp/mixin/TitleScreenMixin.kt
+++ src/main/kotlin/com/github/pozitp/mixin/TitleScreenMixin.kt
@@ -2,17 +2,19 @@ package com.github.pozitp.mixin
 
 import com.github.pozitp.Util.Companion.shownUpdateNotification
 import com.hughbone.fabrilousupdater.platform.ModPlatform
+import net.minecraft.client.gui.screen.Screen
 import net.minecraft.client.gui.screen.TitleScreen
+import net.minecraft.client.util.math.MatrixStack
+import net.minecraft.text.Text
 import org.spongepowered.asm.mixin.Mixin
 import org.spongepowered.asm.mixin.injection.At
 import org.spongepowered.asm.mixin.injection.Inject
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo
 
-
 @Mixin(TitleScreen::class)
-class ModMenuNotificationCheck {
+open class TitleScreenMixin protected constructor(title: Text?) : Screen(title) {
     @Inject(at = [At("TAIL")], method = ["render"])
-    fun init(info: CallbackInfo) {
+    fun render(matrices: MatrixStack, mouseX: Int, mouseY: Int, delta: Float, ci: CallbackInfo) {
         if (shownUpdateNotification) {
             return
         }
